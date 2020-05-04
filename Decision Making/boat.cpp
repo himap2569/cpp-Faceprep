12977c4c1bfa38cd5924e1bd30e5d39498915a95
himap2569 <60684923+himap2569@users.noreply.github.com>
2020-05-04 15:51:21 +0530

    boat.cpp

--- Decision Making/boat.cpp
+++ Decision Making/boat.cpp
@@ -1,5 +1,10 @@
-Boating
-You went on a tour to Ooty with your friends. As a part of the tour, you went boating with them. For the boat to remain stable, the number of people on one boat is restricted based on the weight of the people. You find that the boatman who is sailing your boat is so much greedy of money. For earning more, he takes too many people to travel in the boat at a time. So you want to check how many people can travel in the boat at a time so that the boat will not drown. Calculate the weight by considering the number of adults and number of children. Assume that an adult weighs 75 kg and children weigh 30 kg each. If the weight is normal, display Boat is stable, else display Boat will drown.
+/*Boating
+You went on a tour to Ooty with your friends. As a part of the tour, you went boating with them. For the boat to remain stable, 
+the number of people on one boat is restricted based on the weight of the people. You find that the boatman who is sailing your 
+boat is so much greedy of money. For earning more, he takes too many people to travel in the boat at a time. So you want to check
+how many people can travel in the boat at a time so that the boat will not drown. Calculate the weight by considering the number
+of adults and number of children. Assume that an adult weighs 75 kg and children weigh 30 kg each. If the weight is normal, display
+Boat is stable, else display Boat will drown.
 INPUT & OUTPUT FORMAT:
 
 Input consists of 3 integers.
@@ -21,6 +26,7 @@ SAMPLE INPUT:
 SAMPLE OUTPUT: 
 
 Boat is stable
+*/
 
 #include<iostream>
 using namespace std;
