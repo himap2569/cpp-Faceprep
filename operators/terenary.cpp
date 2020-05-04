bc1546120dcb11ef39142b44df2d5c80954af218
himap2569 <60684923+himap2569@users.noreply.github.com>
2020-05-04 14:20:41 +0530

    Create terenary.cpp

---
+++ operators/terenary.cpp
@@ -0,0 +1,24 @@
+/*
+Ternary Operator
+A conditional operator is also known as ternary operator [exp ? true : false] which can be used to make an either-or choice. Write a C++ program to get a number from the user and find out whether it is odd or even.
+INPUT & OUTPUT FORMAT:
+
+The input is an integer denoting the given number.
+
+If the given number is even, print "Even". Otherwise, print "Odd".
+
+SAMPLE INPUT & OUTPUT:
+
+5
+
+Odd
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+  //Type your code here.4
+  int n;
+  std::cin>>n;
+  n%2==0?std::cout<<"Even":std::cout<<"Odd";
+}
