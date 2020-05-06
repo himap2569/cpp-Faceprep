66c22fd028a88751dc99fbcbd5d1bc319b17ebf5
himap2569 <60684923+himap2569@users.noreply.github.com>
2020-05-06 21:56:15 +0530

    Update Chalk.cpp

--- Loops/Chalk.cpp
+++ Loops/Chalk.cpp
@@ -1,26 +1,34 @@
 /*
 Chalk
-Raj's teacher Anu uses chalk to write on the board. When the chalk reduces to 1/squareroot(n) of its original size, she keeps the chalk aside as it gets too small. She joins all the small pieces of the chalk and makes another chalk of the same size and uses it. If she uses one chalk each day, in how many days will she use the given n number of chalks?
+Raj's teacher Anu uses chalk to write on the board. When the chalk reduces to 1/squareroot(n) of its original size, 
+she keeps the chalk aside as it gets too small. She joins all the small pieces of the chalk and makes another chalk of
+the same size and uses it. If she uses one chalk each day, in how many days will she use the given n number of chalks?
 INPUT & OUTPUT FORMAT:
 
-Input consists of 1 integer which corresponds to the number of chalks given. Output corresponds to the number of days in which the given number of chalks will be used.
+Input consists of 1 integer which corresponds to the number of chalks given. Output corresponds to the number of days 
+in which the given number of chalks will be used.
 
-SAMPLE INPUT:#include<iostream>
-#include<cmath>
-using namespace std;
-int main()
-{
-  //Type your code here.
-  int n;
-  std::cin>>n;
-  n=n/(1-(1/sqrt(n)));
-  std::cout<<n;
-  return 0;
-}
-
+SAMPLE INPUT:\
 16
 
 SAMPLE OUTPUT:
 
 21
 */
+
+#include <iostream>
+using namespace std;
+int main()
+{
+int number=0, days, cycle=50;
+float sqroot=1;
+cin>>number;
+while(cycle>0)
+{
+sqroot = (sqroot+(number/sqroot))/2;
+--cycle;
+}
+days=number+(int(sqroot)+1);
+cout<<int(days);
+return 0;
+}
