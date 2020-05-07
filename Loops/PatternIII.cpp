d52f1f1393d053d42bda1570992b9f1b6a25ff79
himap2569 <60684923+himap2569@users.noreply.github.com>
2020-05-07 12:22:44 +0530

    Create PatternIII.cpp

---
+++ Loops/PatternIII.cpp
@@ -0,0 +1,40 @@
+/*
+Pattern III
+Write a program to print the following pattern.
+Sample Input:
+
+5
+
+Sample Output:
+
+1 
+2*2 
+3*3*3 
+4*4*4*4 
+5*5*5*5*5 
+5*5*5*5*5 
+4*4*4*4 
+3*3*3 
+2*2 
+1
+*/
+#include<iostream>
+int main(){
+  int n;
+  std::cin>>n;
+  for(int i=1;i<=n;i++){
+    for(int j=1;j<=i-1;j++){
+      std::cout<<i<<"*";}
+    std::cout<<i<<"\n";
+  }
+  int i=4;
+  int j=4;
+   for(int i=n;i>=1;i--){
+    for(int j=i-1;j>=1;j--){
+      std::cout<<i<<"*";}
+    std::cout<<i<<"\n";
+  }
+  
+  
+  
+}
