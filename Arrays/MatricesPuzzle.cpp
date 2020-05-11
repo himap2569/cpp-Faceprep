d54b5339420d3bb997d69b15f1dd9b9e6d8bd9fc
himap2569 <60684923+himap2569@users.noreply.github.com>
2020-05-11 12:40:48 +0530

    Create MatricesPuzzle.cpp

---
+++ Arrays/MatricesPuzzle.cpp
@@ -0,0 +1,54 @@
+/*
+Puzzle
+Raju has a square-shaped puzzle made up of small square pieces containing numbers on them. He wants to rearrange the
+puzzle by changing the elements of a row into a column element and column element into a row element. Help Raju to solve
+this puzzle.
+Input Format:
+
+The first input contains an integer 'r' which denotes the number of rows in puzzle
+
+The second input contains an integer 'c' which denotes the number of columns in puzzle
+
+The remaining input denotes the numbers on square shaped puzzle
+
+Output Format:
+
+Print the solved puzzle
+
+Sample Input:
+
+2
+
+2
+
+2 3
+
+4 5
+
+Sample Output:
+
+2 4
+
+3 5
+
+
+*/
+#include<iostream>
+int main()
+{
+    // Type your code here
+  int r,c;
+  std::cin>>r>>c;
+  int arr[r][c];
+  for(int i=0;i<r;i++){
+    for(int j=0;j<c;j++){
+      std::cin>>arr[i][j];
+    }
+  }
+ for(int i=0;i<c;i++){
+    for(int j=0;j<r;j++){
+      std::cout<<arr[j][i]<<" ";
+    }
+   std::cout<<"\n";
+  }
+}
