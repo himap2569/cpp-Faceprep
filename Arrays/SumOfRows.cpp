a0607d750108b41550dd07736d40ef71e977ed15
himap2569 <60684923+himap2569@users.noreply.github.com>
2020-05-13 16:01:09 +0530

    Create SumOfRows.cpp

---
+++ Arrays/SumOfRows.cpp
@@ -0,0 +1,48 @@
+/*
+Sum of rows
+Seenu have a fruit shop and arranged the some set of fruits in column and row wise. Seenu needs to find the total 
+number of fruits in each rows. Help him to find out.
+INPUT & OUTPUT FORMAT:
+
+Input consists of 2 integers and 1 2D-array. Integers correspond to the size of rows and columns.
+
+SAMPLE INPUT & OUTPUT:
+
+3
+
+3
+
+1 2 3
+
+7 3 1
+
+7 4 1
+
+6
+
+11
+
+12
+*/
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+  //Type your code here.
+  int r,c;
+  std::cin>>r>>c;
+  int a[r][c];
+  int max=0;
+    for(int i=0;i<r;i++){
+      max=0;
+    for(int j=0;j<c;j++){
+      std::cin>>a[i][j];
+      max+=a[i][j];
+    }
+    
+      std::cout<<max<<"\n";
+  }
+  
+}
